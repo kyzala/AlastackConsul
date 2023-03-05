65ba9dc145c9b2aaf87eb8365c299e2cb3af5f06
kyzala <10436651+kyzala@users.noreply.github.com>
2023-03-05 11:26:54 +0800

    Add ConsulValidateOptions

---
+++ src/Alastack.Consul/ConsulValidateOptions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alastack.Consul
+{
+    /// <summary>
+    /// Custom implementation for validating <see cref="ConsulOptions"/>.
+    /// </summary>
+    public class ConsulValidateOptions : IValidateOptions<ConsulOptions>
+    {
+        /// <summary>
+        ///  Invoked to validate a <see cref="ConsulOptions"/> instance.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ConsulOptions"/> instance being validated.</param>
+        /// <param name="options">The <see cref="ConsulOptions"/> instance to validate.</param>
+        /// <returns>The <see cref="ValidateOptionsResult"/> result.</returns>
+        public ValidateOptionsResult Validate(string? name, ConsulOptions options)
+        {
+            if (options.Agent == null)
+            {
+                return ValidateOptionsResult.Fail("Agent is null.");
+            }
+            if (options.Registration == null && options.Configuration == null)
+            {
+                return ValidateOptionsResult.Fail("Registration and Configuration are both null.");
+            }
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
