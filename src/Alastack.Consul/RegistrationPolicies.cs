dd75a3c86c8ad02c6c612f5bb96766de6f26b6a7
kyzala <10436651+kyzala@users.noreply.github.com>
2023-03-23 19:46:24 +0800

    Update namespace

--- src/Alastack.Consul/RegistrationPolicies.cs
+++ src/Alastack.Consul/RegistrationPolicies.cs
@@ -4,30 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Alastack.Consul
+namespace Alastack.Consul;
+
+internal static class RegistrationPolicies
 {
-    internal static class RegistrationPolicies
+    public const string RegistrationIdPolicy = nameof(RegistrationIdPolicy);
+
+    public const string HealthCheckIdPolicy = nameof(HealthCheckIdPolicy);
+
+    public const string HealthCheckNamePolicy = nameof(HealthCheckNamePolicy);
+}
+
+internal static class RegistrationPolicyValues
+{
+    public const string Default = "Default";
+
+    public const string ByConsul = "ByConsul";
+
+    public static bool IsByConsul(string policyValue)
     {
-        public const string RegistrationIdPolicy = nameof(RegistrationIdPolicy);
-
-        public const string HealthCheckIdPolicy = nameof(HealthCheckIdPolicy);
-
-        public const string HealthCheckNamePolicy = nameof(HealthCheckNamePolicy);
-    }
-
-    internal static class RegistrationPolicyValues
-    {
-        public const string Default = "Default";
-
-        public const string ByConsul = "ByConsul";
-
-        public static bool IsByConsul(string policyValue)
+        if (string.IsNullOrWhiteSpace(policyValue))
         {
-            if (string.IsNullOrWhiteSpace(policyValue))
-            {
-                return false;
-            }
-            return String.Equals(ByConsul, policyValue, StringComparison.OrdinalIgnoreCase);
+            return false;
         }
+        return String.Equals(ByConsul, policyValue, StringComparison.OrdinalIgnoreCase);
     }
 }
