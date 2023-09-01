9acfc329ed547334ba15dec56e878f19496e2c06
kyzala <10436651+kyzala@users.noreply.github.com>
2023-09-01 20:53:10 +0800

    Update Program.cs

--- samples/AspNetSample/Program.cs
+++ samples/AspNetSample/Program.cs
@@ -4,8 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("consul.json", false, true);
 // Add Consul Configuration
 builder.Configuration.AddConsulConfiguration();
-// Add Consul 
-builder.Services.AddConsul(builder.Configuration);
+// Add Consul Naming
+builder.Services.AddConsulNaming(builder.Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
