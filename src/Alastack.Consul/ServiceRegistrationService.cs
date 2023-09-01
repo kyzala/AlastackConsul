6bb6e6afdb1c5c6da729e324f3ef8ef4feea4ace
kyzala <10436651+kyzala@users.noreply.github.com>
2023-09-01 21:27:36 +0800

    Allow ConsulOptions.Registration null value.

--- src/Alastack.Consul/ServiceRegistrationService.cs
+++ src/Alastack.Consul/ServiceRegistrationService.cs
@@ -14,52 +14,58 @@ public class ServiceRegistrationService : IHostedService
     /// <summary>
     /// Initializes a new instance of <see cref="ServiceRegistrationService"/>.
     /// </summary>
-    /// <param name="options"><see cref="ConsulOptions"/></param>
-    /// <exception cref="ArgumentNullException"><see cref="ConsulOptions.Registration"/> can not be null.</exception>
+    /// <param name="options"><see cref="ConsulOptions"/></param>    
     public ServiceRegistrationService(IOptions<ConsulOptions> options) 
     {
-        if (options.Value.Registration == null) 
-        {
-            throw new ArgumentNullException(nameof(IOptions<ConsulOptions>.Value.Registration));
-        }
+        //if (options.Value.Registration == null) 
+        //{
+        //    throw new ArgumentNullException(nameof(IOptions<ConsulOptions>.Value.Registration));
+        //}
         _consulOptions = options.Value;
     }
 
     /// <inheritdoc />
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        var registration = _consulOptions.Registration!;
-        var consulClient = CreateConsulClient(_consulOptions.Agent);
-
-        await consulClient.Agent.ServiceRegister(new AgentServiceRegistration()
+        if (_consulOptions.Registration != null)
         {
-            ID = registration.Id,
-            Name = registration.Name,
-            Address = registration.Address.Host,
-            Port = registration.Address.Port,
-            Tags = registration.Tags,
-            Meta = registration.Metadata,
-            EnableTagOverride = registration.EnableTagOverride,
-            Check = new AgentServiceCheck
+            var registration = _consulOptions.Registration!;
+
+            var consulClient = CreateConsulClient(_consulOptions.Agent);
+
+            await consulClient.Agent.ServiceRegister(new AgentServiceRegistration()
             {
-                CheckID = registration.HealthCheck.CheckId,
-                Name = registration.HealthCheck.Name,
-                DeregisterCriticalServiceAfter = registration.HealthCheck.DeregisterCriticalServiceAfter,
-                Interval = registration.HealthCheck.Interval,
-                HTTP = registration.HealthCheck.Health.ToString(),
-                Timeout = registration.HealthCheck.Timeout
-            }
-            //Checks = registration.Checks
-        }, cancellationToken);
-        consulClient.Dispose();
+                ID = registration.Id,
+                Name = registration.Name,
+                Address = registration.Address.Host,
+                Port = registration.Address.Port,
+                Tags = registration.Tags,
+                Meta = registration.Metadata,
+                EnableTagOverride = registration.EnableTagOverride,
+                Check = new AgentServiceCheck
+                {
+                    CheckID = registration.HealthCheck.CheckId,
+                    Name = registration.HealthCheck.Name,
+                    DeregisterCriticalServiceAfter = registration.HealthCheck.DeregisterCriticalServiceAfter,
+                    Interval = registration.HealthCheck.Interval,
+                    HTTP = registration.HealthCheck.Health.ToString(),
+                    Timeout = registration.HealthCheck.Timeout
+                }
+                //Checks = registration.Checks
+            }, cancellationToken);
+            consulClient.Dispose();
+        }
     }
 
     /// <inheritdoc />
     public async Task StopAsync(CancellationToken cancellationToken)
-    {            
-        var consulClient = CreateConsulClient(_consulOptions.Agent); 
-        await consulClient.Agent.ServiceDeregister(_consulOptions.Registration!.Id, cancellationToken);
-        consulClient.Dispose();
+    {
+        if (_consulOptions.Registration != null)
+        {
+            var consulClient = CreateConsulClient(_consulOptions.Agent);
+            await consulClient.Agent.ServiceDeregister(_consulOptions.Registration!.Id, cancellationToken);
+            consulClient.Dispose();
+        }
     }
 
     // Create ConsulClient instance.
