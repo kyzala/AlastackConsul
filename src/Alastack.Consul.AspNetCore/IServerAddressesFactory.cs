7f6ba9d412d1f0168a8f48ce448299a2e6501d3d
kyzala <10436651+kyzala@users.noreply.github.com>
2023-09-06 21:28:48 +0800

    Add Alastack.Consul.AspNetCore

---
+++ src/Alastack.Consul.AspNetCore/IServerAddressesFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Alastack.Consul.AspNetCore;
+
+public interface IServerAddressesFactory
+{
+    bool Configuered { get; set; }
+
+    void AddServerAddress(string address);
+
+    ICollection<string> GetServerAddresses();
+}
---
+++ src/Alastack.Consul.AspNetCore/ServerAddressesFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Alastack.Consul.AspNetCore;
+
+public class ServerAddressesFilter : IStartupFilter
+{
+    private readonly IServer _server;
+    private readonly IConfiguration _configuration;
+    public readonly IServerAddressesFactory _serverAddressesFactory;
+
+    public ServerAddressesFilter(IServer server, IConfiguration configuration, IServerAddressesFactory serverAddressesFactory)
+    {
+        _server = server;
+        _configuration = configuration;
+        _serverAddressesFactory = serverAddressesFactory;
+    }
+
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        var serverAddressesFeature = _server.Features.Get<IServerAddressesFeature>();
+        if (serverAddressesFeature?.Addresses != null) 
+        {
+            foreach (var address in serverAddressesFeature.Addresses) 
+            {
+                _serverAddressesFactory.AddServerAddress(address);
+            }
+        }
+        _serverAddressesFactory.Configuered = true;
+
+        return next;
+    }
+}
