0c578ee3a3a2fff52466bf70f31162c45363c10a
kyzala <10436651+kyzala@users.noreply.github.com>
2023-09-11 21:34:21 +0800

    Add dynamic instance Sample project

---
+++ samples/DynamicInstanceSample/ApplicationBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace DynamicInstanceSample;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app, PathString path)
+    {
+        app.Map(path, builder => builder.Use(
+            (HttpContext context, RequestDelegate next) =>
+            {
+                context.Response.StatusCode = 200;
+                return context.Response.WriteAsync("healthy");
+            }));
+        return app;
+    }
+
+    public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app, PathString path, Func<IServiceProvider, bool> healthCheckFunc)
+    {
+        if (healthCheckFunc == null)
+        {
+            healthCheckFunc = serviceProvider => true;
+        }
+        return UseHealthCheck(app, path, serviceProvider => Task.FromResult(healthCheckFunc(serviceProvider)));
+    }
+
+    public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app, PathString path, Func<IServiceProvider, Task<bool>> healthCheckFunc)
+    {
+        if (healthCheckFunc == null)
+        {
+            healthCheckFunc = serviceProvider => Task.FromResult(true);
+        }
+        app.Map(path, builder => builder.Use(
+            async (HttpContext context, RequestDelegate next) =>
+            {
+                var healthCheckResult = "unhealthy";
+                try
+                {
+                    var isHealthy = await healthCheckFunc.Invoke(context.RequestServices);
+                    if (isHealthy)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status200OK;
+                        healthCheckResult = "healthy";
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = context.RequestServices.GetService<ILoggerFactory>()!.CreateLogger("HealthCheck");
+                    logger.LogError(ex, "Executing HealthCheck Error.");
+                    context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                }
+                finally
+                {
+                    await context.Response.WriteAsync(healthCheckResult);
+                }
+            }));
+        return app;
+    }
+}
---
+++ samples/DynamicInstanceSample/Program.cs
@@ -0,0 +1,17 @@
+using DynamicInstanceSample;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("consul.json", false, true);
+// Add Consul Configuration
+builder.Configuration.AddConsulConfiguration();
+// Add Consul Naming
+builder.Services.AddConsulNaming(builder.Configuration).AddAspNetCore();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseHealthCheck("/health");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
