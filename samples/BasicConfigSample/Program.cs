b9a1d9a3c2b597f8eca589d46dd8e9cea851f8d1
kyzala <10436651+kyzala@users.noreply.github.com>
2026-01-03 17:33:59 +0800

    Update version to 1.3.0

---
+++ samples/BasicConfigSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sample.Common;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+IConfigurationBuilder builder = new ConfigurationBuilder();
+
+builder.AddJsonFile("appsettings.json", false, true);
+builder.AddConsulConfiguration();
+var configuration = builder.Build();
+
+var options = configuration.GetSection("ConfigOptions").Get<ConfigOptions>();
+
+var jsonSerializerOptions = new JsonSerializerOptions 
+{ 
+    WriteIndented = true, 
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull 
+};
+var str = JsonSerializer.Serialize(options, jsonSerializerOptions);
+
+Console.WriteLine("Output ConfigOptions:\n");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write(str);
+
+Console.ReadLine();
